8762e836b396ba8609849a4611fd4f930ee4ca16
Derek Rogers <derekgrogers1@gmail.com>
2023-04-28 12:04:51 -0500

    renamed and resized skill graph. Now displays skill graph on the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,16 +1,5 @@
 <script lang="ts">
-	import { SvelteComponentTyped } from 'svelte';
-	import Image from 'src/lib/images/bloomSkillGraphVisualization.png';
-
-	interface MyImageProps {
-	src: string;
-	alt: string;
-	}
-
-	class MyImage extends SvelteComponentTyped<MyImageProps> {}
-	// let files;
-	//<label for="many">Upload markov chain data file (.csv):</label>
-	//<input id="markovChainDataFile" type="file" bind:files/>
+	import Image from '$lib/images/skillGraph.png';
 </script>
 
 <svelte:head>
@@ -18,4 +7,4 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<MyImage src={Image} alt="image" />
+<img src={Image} alt="image" />
