155a6bd10bd84229ecc8b3cf65ad765cb0fbb6c2
Derek Rogers <derekgrogers1@gmail.com>
2023-04-28 14:20:45 -0500

    updated formatting of skill tree image.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,4 +7,13 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
+
 <img src={Image} alt="image" />
+
+  
+<style>
+	img {
+		transform: scale(1.3);
+		transform-origin: top center;
+	}
+</style>
